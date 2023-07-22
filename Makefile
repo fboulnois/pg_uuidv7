b483f4131da81557a5fdeb02152cf19bc61b7edf
fboulnois <fboulnois@users.noreply.github.com>
2023-07-22 15:09:44 -0400

    feat: bump extension version to 1.1.0

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 MODULES = pg_uuidv7
 EXTENSION = pg_uuidv7
-DATA = pg_uuidv7--1.0.sql
+DATA = pg_uuidv7--1.1.sql
 
 PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
---
+++ pg_uuidv7--1.1.sql
@@ -0,0 +1,14 @@
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use '''CREATE EXTENSION "pg_uuidv7"''' to load this file. \quit
+
+-- 48 bits for ms since unix epoch (rollover in 10899), 74 bits of randomness
+CREATE FUNCTION uuid_generate_v7()
+RETURNS uuid
+AS 'MODULE_PATHNAME', 'uuid_generate_v7'
+VOLATILE STRICT LANGUAGE C PARALLEL SAFE;
+
+-- extract the timestamp from a v7 uuid
+CREATE FUNCTION uuid_v7_to_timestamptz(uuid)
+RETURNS timestamptz
+AS 'MODULE_PATHNAME', 'uuid_v7_to_timestamptz'
+VOLATILE STRICT LANGUAGE C PARALLEL SAFE;
